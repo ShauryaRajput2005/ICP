bed87bcba3e6fdda56c5abcdb83c9600334e24ef
Shikhar Shaurya <shikharshauryarajput@gmail.com>
2025-09-29 23:26:03 +0530

    update

--- ICP/src/Assignment_Day1/SlidingWindowMaximum.java
+++ ICP/src/Assignment_Day1/SlidingWindowMaximum.java
@@ -1,10 +1,44 @@
 package Assignment_Day1;
 
+import java.util.Arrays;
+
 public class SlidingWindowMaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 || k == 0) return new int[0];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
 
-	}
+        for (int i = 0; i < n; i++) {
+            if (i % k == 0) {
+                prefix[i] = nums[i];
+            } else {
+                prefix[i] = Math.max(prefix[i - 1], nums[i]);
+            }
+        }
 
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffix[i] = nums[i];
+            } else {
+                suffix[i] = Math.max(suffix[i + 1], nums[i]);
+            }
+        }
+
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i + k - 1 < n; i++) {
+            ans[i] = Math.max(suffix[i], prefix[i + k - 1]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+
+        int[] result = maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(result));  
+    }
 }
